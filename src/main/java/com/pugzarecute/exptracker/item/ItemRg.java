76eee253ce640365dff2f2cb5b47053081f46dd7
PugzAreCute <me@pugzarecute.com>
2023-04-16 11:01:53 +0530

    Begin

---
+++ src/main/java/com/pugzarecute/exptracker/item/ItemRg.java
@@ -0,0 +1,18 @@
+package com.pugzarecute.exptracker.item;
+
+import com.pugzarecute.exptracker.EXPTracker;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemRg {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EXPTracker.MODID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+    }
+    public static final RegistryObject<Item> TRACKING_COMPASS = ITEMS.register("tracking_compass", HunterItem::new);
+}
